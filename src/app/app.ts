c8dcc8ba28c5ca7de5f140c8491ab63b0792e487
petermahfouz22 <peter.mahfouz23@gmail.com>
2025-06-13 14:16:05 -0700

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Test} from './test/test';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,Test],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'test-angular-20';
+}
